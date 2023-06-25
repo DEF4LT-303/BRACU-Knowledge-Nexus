a6336f0c8f3b4f0c334a7e3739a2cdd107bb4c13
DEF4LT-303 <ryanrafi72@gmail.com>
2023-06-26 00:07:18 +0600

    403 forbidden error potentially fixed

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,15 +1,36 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  headers: { token: `${TOKEN}` }
+  baseURL: BASE_URL
 });
+
+// Function to set the token in the request headers
+export const setAuthToken = (token) => {
+  if (token) {
+    userRequest.defaults.headers.common[
+      ('Authorization', 'token')
+    ] = `${token}`;
+  } else {
+    delete userRequest.defaults.headers.common['Authorization'];
+  }
+};
+
+// Intercept requests to set the token
+userRequest.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem('persist:root'))?.user;
+    const currentUser = token && JSON.parse(token).currentUser;
+    const accessToken = currentUser?.accessToken;
+    setAuthToken(accessToken);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
