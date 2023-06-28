f176be0ad0e375db503c022200b4c4af1c904b38
DEF4LT-303 <ryanrafi72@gmail.com>
2023-06-28 18:07:03 +0600

    CORS updated further

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 
-// // Enable CORS for all routes
+// Enable CORS for all routes
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -11,6 +12,8 @@ const dotenv = require('dotenv');
 
 //   next();
 // });
+
+app.use(cors());
 
 // const allowedOrigins = [
 //   'http://localhost:3000',
@@ -26,27 +29,6 @@ const dotenv = require('dotenv');
 //   optionSuccessStatus: 200
 // };
 // app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://knowledge-nexus-lime.vercel.app',
-    'https://knowledge-nexus-oi9p6y342-def4lt-303.vercel.app',
-    'https://knowledge-nexus-static.onrender.com'
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.header('Access-Control-Allow-credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
-  next();
-});
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
