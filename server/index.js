f9476190a546420763da75b5e228fa926cf3a8c5
DEF4LT-303 <ryanrafi72@gmail.com>
2023-06-28 16:44:02 +0600

    CORS origin updated

--- server/index.js
+++ server/index.js
@@ -12,9 +12,14 @@ const dotenv = require('dotenv');
 //   next();
 // });
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://bracu-discussion-forum.vercel.app/'
+];
+
 const cors = require('cors');
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: allowedOrigins,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
