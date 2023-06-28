eb588c3609890da376fca00234475d596857b7c6
DEF4LT-303 <ryanrafi72@gmail.com>
2023-06-28 13:26:13 +0600

    api

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,19 +1,37 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/';
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
-
-const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
+// const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://knowledge-nexus.onrender.com/api/'; // hosted on render.com
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  header: { token: `${TOKEN}` }
+  baseURL: BASE_URL
 });
+
+// Function to set the token in the request headers
+export const setAuthToken = (token) => {
+  if (token) {
+    userRequest.defaults.headers.common[
+      ('Authorization', 'token')
+    ] = `${token}`;
+  } else {
+    delete userRequest.defaults.headers.common['Authorization'];
+  }
+};
+
+// Intercept requests to set the token
+userRequest.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem('persist:root'))?.user;
+    const currentUser = token && JSON.parse(token).currentUser;
+    const accessToken = currentUser?.accessToken;
+    setAuthToken(accessToken);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
