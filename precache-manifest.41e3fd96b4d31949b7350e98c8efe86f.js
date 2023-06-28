f20d692f630e45dc8d41ef9506954508c269b9c4
DEF4LT-303 <ryanrafi72@gmail.com>
2023-06-28 13:10:28 +0600

    Updates

---
+++ precache-manifest.41e3fd96b4d31949b7350e98c8efe86f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c683fb68b88b891a53229c8b31a5eb79",
+    "url": "/BRACU-Discussion-Forum/index.html"
+  },
+  {
+    "revision": "16b5bc3e30f032e42ab9",
+    "url": "/BRACU-Discussion-Forum/static/js/2.47ddc7a2.chunk.js"
+  },
+  {
+    "revision": "b693ab156cc652af9a461fb1739e7305",
+    "url": "/BRACU-Discussion-Forum/static/js/2.47ddc7a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa7e9602cb3d8b61cf64",
+    "url": "/BRACU-Discussion-Forum/static/js/main.fd440762.chunk.js"
+  },
+  {
+    "revision": "69ec1cd2d0560bba8b16",
+    "url": "/BRACU-Discussion-Forum/static/js/runtime-main.83f6bcba.js"
+  }
+]);
