668f48bf80249eb887c3e85937d255ae202cc9ac
maidul02 <maidulislammahin02@gmail.com>
2023-07-12 15:36:24 +0600

    Added Forum

---
+++ server/models/Forum.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const forumSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "A post must have a title"],
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: [true, "A post must have a description"],
+    trim: true,
+  },
+
+  creator: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "A post must have a creator"],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  media: {
+    type: String,
+    required: [false, "A file is must required to create a post!"],
+    trim: true,
+  },
+
+  tags: {
+    type: Array,
+    default: [],
+  },
+
+  upVotes: {
+    type: Array,
+    default: [],
+  },
+
+  downVotes: {
+    type: Array,
+    default: [],
+  },
+
+  comments: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Forum = mongoose.model("Forum", forumSchema);
+module.exports = Forum;
