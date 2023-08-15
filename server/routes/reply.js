0af3001e3d1df8c93339a5e0a11aa119ec229580
maidul02 <maidulislammahin02@gmail.com>
2023-08-15 19:34:10 +0600

    Create a new reply

---
+++ server/routes/reply.js
@@ -0,0 +1,78 @@
+const Reply = require('../models/Reply');
+const { verify, verifyTokenAuth, verifyTokenAdmin } = require('./verifyToken');
+
+const router = require('express').Router();
+
+// Create a new reply 
+router.post('/', verifyTokenAuth, async (req, res) => {
+  const newReply = new Reply(req.body);
+
+  try {
+    const savedReply = await newReply.save();
+    res.status(201).json(savedReply);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all forum posts
+router.get('/', async (req, res) => {
+  try {
+    const forumPosts = await Forum.find()
+      .populate('creator', '-password')
+      .sort({ createdAt: -1 });
+    res.status(200).json(forumPosts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get a single forum post by ID
+router.get('/find/:id', async (req, res) => {
+  try {
+    const forumPost = await Forum.findById(req.params.id).populate(
+      'creator',
+      '-passowrd'
+    );
+    if (!forumPost) {
+      return res.status(404).json({ message: 'Forum post not found' });
+    }
+    res.status(200).json(forumPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a forum post
+router.put('/:id', verifyTokenAuth, async (req, res) => {
+  try {
+    const updatedForumPost = await Forum.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body
+      },
+      { new: true }
+    );
+    if (!updatedForumPost) {
+      return res.status(404).json({ message: 'Forum post not found' });
+    }
+    res.status(200).json(updatedForumPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a forum post
+router.delete('/:id', verifyTokenAuth, async (req, res) => {
+  try {
+    const deletedForumPost = await Forum.findByIdAndDelete(req.params.id);
+    if (!deletedForumPost) {
+      return res.status(404).json({ message: 'Forum post not found' });
+    }
+    res.status(200).json({ message: 'Forum post has been deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
