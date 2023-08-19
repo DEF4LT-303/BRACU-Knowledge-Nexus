647a8336b51d1954782583080f360168c7374ce6
Sami Sadat <samisadat909@gmail.com>
2023-08-19 20:10:23 +0600

    threadreply

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -1,26 +1,41 @@
 const Forum = require('../models/Forum');
+const User = require('../models/User');
+const Reply = require('../models/Reply');
 const { verify, verifyTokenAuth, verifyTokenAdmin } = require('./verifyToken');
 
 const router = require('express').Router();
 
 // Create a new forum post
 router.post('/', verifyTokenAuth, async (req, res) => {
-  const newForumPost = new Forum(req.body);
-
   try {
+    const newForumPost = new Forum(req.body);
     const savedForumPost = await newForumPost.save();
+
+    // Update the user's posts array
+    await User.findByIdAndUpdate(req.user.id, {
+      $addToSet: { posts: savedForumPost._id } // $addToSet ensures no duplicates
+    }).exec();
+
     res.status(201).json(savedForumPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Get all forum posts
+// Get all forum posts with populated replies
 router.get('/', async (req, res) => {
   try {
     const forumPosts = await Forum.find()
+      .populate({
+        path: 'replies',
+        populate: {
+          path: 'creator',
+          select: '-password' // Exclude password field
+        }
+      })
       .populate('creator', '-password')
       .sort({ createdAt: -1 });
+
     res.status(200).json(forumPosts);
   } catch (err) {
     res.status(500).json(err);
@@ -30,10 +45,15 @@ router.get('/', async (req, res) => {
 // Get a single forum post by ID
 router.get('/find/:id', async (req, res) => {
   try {
-    const forumPost = await Forum.findById(req.params.id).populate(
-      'creator',
-      '-passowrd'
-    );
+    const forumPost = await Forum.findById(req.params.id)
+      .populate({
+        path: 'replies',
+        populate: {
+          path: 'creator',
+          select: '-password' // Exclude password field
+        }
+      })
+      .populate('creator', '-password');
     if (!forumPost) {
       return res.status(404).json({ message: 'Forum post not found' });
     }
@@ -64,13 +84,45 @@ router.put('/:id', verifyTokenAuth, async (req, res) => {
 
 // Delete a forum post
 router.delete('/:id', verifyTokenAuth, async (req, res) => {
+  const forumID = req.params.id;
+
   try {
-    const deletedForumPost = await Forum.findByIdAndDelete(req.params.id);
-    if (!deletedForumPost) {
-      return res.status(404).json({ message: 'Forum post not found' });
+    const forum = await Forum.findById(forumID);
+    if (!forum) {
+      return res.status(404).json('Forum not found.');
     }
-    res.status(200).json({ message: 'Forum post has been deleted' });
+
+    const isAdmin = req.user.role === 'admin';
+    const isCreator = forum.creator.toString() === req.user.id;
+
+    if (!isAdmin && !isCreator) {
+      return res.status(403).json('You are not allowed to delete this forum.');
+    }
+
+    await Promise.all(
+      forum.replies.map(async (reply) => {
+        await Reply.findByIdAndDelete(reply._id);
+      })
+    );
+
+    if (isCreator) {
+      await User.findByIdAndUpdate(req.user.id, {
+        $pull: { posts: forumID },
+        $inc: { postsCount: -1 }
+      }).exec();
+    }
+
+    if (isAdmin && !isCreator) {
+      await User.findByIdAndUpdate(forum.creator, {
+        $pull: { posts: forumID },
+        $inc: { postsCount: -1 }
+      }).exec();
+    }
+
+    await Forum.deleteOne({ _id: forumID });
+    return res.status(200).json('Forum deleted.');
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
