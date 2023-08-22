5452b0a3da72c989e4d01286a339b70e1970bee9
maidul02 <maidulislammahin02@gmail.com>
2023-08-22 19:48:58 +0600

    Create a new Feedback post

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -4,3 +4,14 @@ const { verify, verifyTokenAuth, verifyTokenAdmin } = require('./verifyToken');
 
 const router = require('express').Router();
 
+// Create a new Feedback post
+router.post('/', verifyTokenAuth, async (req, res) => {
+  try {
+    const newFeedbackPost = new Feedback(req.body);
+    const savedFeedbackPost = await newFeedbackPost.save();
+
+    res.status(201).json(savedFeedbackPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
