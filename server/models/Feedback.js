93243e85c1c63a267aa9125601870da945b70518
maidul02 <maidulislammahin02@gmail.com>
2023-08-22 19:46:25 +0600

    added description

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const feedbackSchema = new mongoose.Schema({
+
+  description: {
+    type: String,
+    required: [true, 'A Feedback must have a description'],
+    trim: true
+  }
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+module.exports = Feedback;
